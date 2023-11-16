e9ddbac7639724227549285c4166bc242019d4d2
jacob0x2 <fantaciveEvolution1248@gmail.com>
2023-11-16 19:05:00 +0900

    update

---
+++ fix_user.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter '
+NEW_NAME="jacob0x2"
+NEW_EMAIL="fantaciveEvolution1248@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" != "$NEW_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" != "$NEW_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
